441abe4d7a70aef117c10f39a7bb2e319237244e
anutthara <anutthar@gmail.com>
2017-06-12 09:30:27 +1000

    Adding files

---
+++ Mocha/Mocha/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mocha
+//
+//  Created by Anu Bharadwaj on 6/9/17.
+//  Copyright © 2017 Anu Bharadwaj. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
